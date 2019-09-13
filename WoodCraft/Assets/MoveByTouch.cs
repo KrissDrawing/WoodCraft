ebf5915b8718d3e578849cc28975df466b54afda
KrissDrawing <krx95@o2.pl>
2019-09-13 12:41:04 +0200

    Simple movement
    tap to move

---
+++ WoodCraft/Assets/MoveByTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+    Touch touch;
+    Vector3 touchPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+        }
+        transform.position = Vector3.Lerp(transform.position, touchPosition, 0.025f);
+    }
+}
